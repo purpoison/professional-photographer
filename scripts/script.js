f476ff0e5b54f3e8b8542bc0a4d73fea45428fe8
DannydeLimero <dendelime@gmail.com>
2022-08-07 12:15:52 +0300

    new

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:2500,
+        pauseOnFocus:true,
+        pauseOnHover:true,
+        pauseOnDotsHover:true,
+        touchThreshold:10,
+        touchmove:true,
+        waitForAnimate:false,
+        infinite:true,
+    });
+});
